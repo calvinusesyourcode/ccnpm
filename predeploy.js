e5cacc756753fd0c7a40bb60d7a3b8b89ab7f2da
Calvin Ducharme <calvinducharme@gmail.com>
2024-06-08 17:41:52 -0700

    ^

--- predeploy.js
+++ predeploy.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const date = new Date()
 date.setHours(date.getHours() - 8)
-const version = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}.${date.getHours()}${date.getMinutes()}${date.getSeconds()}`
+const version = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`
 const packagePath = path.join(__dirname, 'package.json')
 const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
 packageJson.version = version
